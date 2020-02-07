80efdf56931738bf7fe7883b02beca1267e0ce72
llarruda <leandroarruda1977@gmail.com>
2020-02-07 17:12:59 -0300

    Adapted to MVC pattern, set up nodemon as dev dependency, and added README file.

---
+++ README.md
@@ -0,0 +1,14 @@
+# express-app
+
+#### Node.js app with Express lightweight framework
+
+## Environment
+
+#### Runtime Environment
+###### Node [v13.8.0]
+
+#### Node Package Manager
+###### Yarn Classic [v1.21.1]
+
+#### Run it
+###### DEBUG=express-app:* yarn dev
--- app.js
+++ app.js
@@ -1,26 +1,33 @@
+'use strict';
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// require file-system module, indicated to easy perform operatons related to files
+var fs = require('file-system');
 
 var app = express();
 
+// include controllers
+fs.readdirSync('controllers').forEach( (file) => {
+  if (file.substr(-3) == '.js') {
+    const route = require('./controllers/' + file);
+    route.controller(app);
+  }
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
